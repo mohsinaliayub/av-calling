e2eda8ce9516f597a9c011e61d9871902ae42fd8
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-08-04 14:12:35 +0200

    Initial Commit

---
+++ AzCommunicationService/AzCommunicationServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AzCommunicationServiceApp.swift
+//  AzCommunicationService
+//
+//  Created by Mohsin Ali Ayub on 04.08.24.
+//
+
+import SwiftUI
+
+@main
+struct AzCommunicationServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AzCommunicationService/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AzCommunicationService
+//
+//  Created by Mohsin Ali Ayub on 04.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
